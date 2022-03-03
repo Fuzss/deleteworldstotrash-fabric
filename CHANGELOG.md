d197d5f0128d4e0fb7eb6838bc6fe5dc60f2f7e9
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-03-03 22:57:22 +0100

    compiled for 1.18.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog].
 
+## [v3.2.0-1.18.2] - 2022-03-03
+- Compiled for Minecraft 1.18.2
+
 ## [v3.1.0-1.18.1] - 2022-01-25
 ### Added
 - Added a notification when a world has been deleted instead of moved to the trash due to the system being unsupported
--- src/main/java/fuzs/deleteworldstotrash/DeleteWorldsToTrash.java
+++ src/main/java/fuzs/deleteworldstotrash/DeleteWorldsToTrash.java
@@ -1,10 +1,10 @@
 package fuzs.deleteworldstotrash;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DeleteWorldsToTrash {
     public static final String MOD_ID = "deleteworldstotrash";
     public static final String MOD_NAME = "Delete Worlds To Trash";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 }
